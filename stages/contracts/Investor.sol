f6c5279c45b3ae6d16c0ec1ab60966a3c44b149f
yqsy <yqsy021@126.com>
2018-09-11 10:48:23 +0800

    重新梳理逻辑

--- stages/contracts/Investor.sol
+++ stages/contracts/Investor.sol
@@ -14,6 +14,23 @@ library Investor {
         State state;            // 状态
     }
 
+    // 创建新投资者
+    function newInvestor(address _id, uint256 _investABSNum, uint256 _itemTokenNum) internal pure
+    returns (Investor_) {
+        Investor.Investor_ memory investor;
+        investor.id = _id;
+        investor.investABSNum = _investABSNum;
+        investor.itemTokenNum = _itemTokenNum;
+        investor.state = State.UnVoted;
+        return investor;
+    }
+
+    // 追加投资
+    function appendInvest(Investor_ storage investor, uint256 investABSNum, uint256 itemTokenNum) internal {
+        investor.investABSNum = investor.investABSNum.add(investABSNum);
+        investor.itemTokenNum = investor.itemTokenNum.add(itemTokenNum);
+    }
+
     // 寻找投资者是否存在
     function findInIdx(Investor_[] storage investors, address id) internal view
     returns (bool, uint256) {
@@ -25,39 +42,19 @@ library Investor {
         return (false, 0);
     }
 
-    // 追加投资
-    function appendInvestor(Investor_ storage investor, uint256 investABSNum, uint256 itemTokenNum) internal {
-        investor.investABSNum = investor.investABSNum.add(investABSNum);
-        investor.itemTokenNum = investor.itemTokenNum.add(itemTokenNum);
-    }
-
-    // 创建新投资者
-    function newInvestor(address _id, uint256 _investABSNum, uint256 _itemTokenNum) internal pure
-    returns (Investor_) {
-        Investor.Investor_ memory investor;
-        investor.id = _id;
-        investor.investABSNum = _investABSNum;
-        investor.itemTokenNum = _itemTokenNum;
-
-        investor.state = State.UnVoted;
-        return investor;
-    }
-
+    // 是否已投票
     function isVoted(Investor_ storage investor) internal view
     returns (bool) {
         return investor.state != State.UnVoted;
     }
 
+    // 是否同意
     function isAgreed(Investor_ storage investor) internal view
     returns (bool) {
         return investor.state == State.Agree;
     }
 
-    function isOppose(Investor_ storage investor) internal view
-    returns (bool) {
-        return investor.state == State.Oppose;
-    }
-
+    // 投票
     function vote(Investor_ storage investor, bool agree) internal {
         if (agree) {
             investor.state = State.Agree;
@@ -80,7 +77,7 @@ library Investor {
         return agreeRate >= targetAgreeRate.mul(100);
     }
 
-    // (投票成功): 投资者领取众筹阶段的所有应得到的token,并减去合同记录
+    // 投资者: (投票成功) 获取token
     function investorWithdrawToken(Investor_[] storage investors, function (address/*id*/, uint256/*tokenNum*/) addInvestorBalance) internal  {
         for (uint256 i = 0; i < investors.length; i++) {
             addInvestorBalance(investors[i].id, investors[i].itemTokenNum);
@@ -88,15 +85,7 @@ library Investor {
         }
     }
 
-    // (投票成功): 项目方领取众筹阶段的所有应得的ABS,并减去合同记录
-    function itemWithdrawABS(Investor_[] storage investors, function (uint256/*ABSNum*/) addItemABS) internal  {
-        for (uint256 i = 0; i < investors.length; i++) {
-            addItemABS(investors[i].investABSNum);
-            investors[i].investABSNum = 0;
-        }
-    }
-
-    // (投票失败): 投资者领取众筹阶段付出的ABS,并减去合同记录
+    // 投资者: (投票失败) 获取ABS
     function investorWithdrawABS(Investor_[] storage investors, function (uint256/*ABSNum*/) addInvestorABS) internal {
         for (uint256 i = 0; i < investors.length; i++) {
             addInvestorABS(investors[i].investABSNum);
@@ -104,7 +93,15 @@ library Investor {
         }
     }
 
-    // (投票失败): 项目方领取众筹阶段付出的token,并减去合同记录
+    // 项目方: (投票成功) 获取ABS
+    function itemWithdrawABS(Investor_[] storage investors, function (uint256/*ABSNum*/) addItemABS) internal  {
+        for (uint256 i = 0; i < investors.length; i++) {
+            addItemABS(investors[i].investABSNum);
+            investors[i].investABSNum = 0;
+        }
+    }
+
+    // 项目方: (投票失败) 获取token
     function itemWithdrawToken(Investor_[] storage investors, function (uint256/*tokenNum*/) addItemBalance) internal {
         for (uint256 i = 0; i < investors.length; i++) {
             addItemBalance(investors[i].itemTokenNum);
--- stages/contracts/StagesToken.sol
+++ stages/contracts/StagesToken.sol
@@ -38,9 +38,9 @@ contract StagesToken is ERC20, ERC20Detailed, ERC20Burnable {
 
     // 众筹期: 投资者投资ABS
     function Invest() public payable {
-        require(StageTime.isInSale(_stages[_currentStageIdx].stageTime, now));
-
         Stage storage curStage = _stages[_currentStageIdx];
+        require(StageTime.isInSale(curStage.stageTime, now));
+
         (bool exist, uint256 idx) = Investor.findInIdx(curStage.investors, msg.sender);
 
         // 1. A. 项目方token减少 B. 合同token增加
@@ -50,7 +50,7 @@ contract StagesToken is ERC20, ERC20Detailed, ERC20Burnable {
         // 2. A. 投资者ABS减少 B. 合同ABS增加
         if (exist) {
             Investor.Investor_ storage curInvestor = curStage.investors[idx];
-            Investor.appendInvestor(curInvestor, msg.value, rateTokenNum);
+            Investor.appendInvest(curInvestor, msg.value, rateTokenNum);
         } else {
             Investor.Investor_ memory investor = Investor.newInvestor(msg.sender, msg.value, rateTokenNum);
             curStage.investors.push(investor);
@@ -59,10 +59,10 @@ contract StagesToken is ERC20, ERC20Detailed, ERC20Burnable {
 
     // 投票期: 投资者投票 0=不同意,其他=同意
     function Vote(uint256 isAgree) public {
-        require(StageTime.isInVote(_stages[_currentStageIdx].stageTime, now));
+        Stage storage curStage = _stages[_currentStageIdx];
+        require(StageTime.isInVote(curStage.stageTime, now));
 
         // 必须参加过众筹
-        Stage storage curStage = _stages[_currentStageIdx];
         (bool exist, uint256 idx) = Investor.findInIdx(curStage.investors, msg.sender);
         require(exist);
 
@@ -78,14 +78,13 @@ contract StagesToken is ERC20, ERC20Detailed, ERC20Burnable {
         }
     }
 
-    // 结束后: 参照投票结果(成功),投资者领取众筹阶段的所有应得的token
+    // 投资者: (投票成功) 获取token
     function InvestorWithdrawToken() public {
-        require(StageTime.isEnd(_stages[_currentStageIdx].stageTime, now));
+        Stage storage curStage = _stages[_currentStageIdx];
+        require(StageTime.isEnd(curStage.stageTime, now));
 
         // 只能是投资者
         require(msg.sender != _item);
-
-        Stage storage curStage = _stages[_currentStageIdx];
 
         // 当期投票同意
         require(Investor.isVoteAgreeAchieveTarget(curStage.investors, curStage.targetAgreeRate));
@@ -93,29 +92,13 @@ contract StagesToken is ERC20, ERC20Detailed, ERC20Burnable {
         Investor.investorWithdrawToken(curStage.investors, addBalance);
     }
 
-    // 结束后:参照投票结果(成功),项目方领取众筹阶段的所有应得的ABS
-    function ItemWithdrawABS() public {
-        require(StageTime.isEnd(_stages[_currentStageIdx].stageTime, now));
-
-        // 只能是项目方
-        require(msg.sender == _item);
-
-        Stage storage curStage = _stages[_currentStageIdx];
-
-        // 当前投票同意
-        require(Investor.isVoteAgreeAchieveTarget(curStage.investors, curStage.targetAgreeRate));
-
-        Investor.itemWithdrawABS(curStage.investors, addSendersABS);
-    }
-
-    // 结束后: 参照投票结果(失败),投资者领取众筹阶段付出的ABS
+    // 投资者: (投票失败) 获取ABS
     function InvestorWithdrawAbs() public {
-        require(StageTime.isEnd(_stages[_currentStageIdx].stageTime, now));
+        Stage storage curStage = _stages[_currentStageIdx];
+        require(StageTime.isEnd(curStage.stageTime, now));
 
         // 只能是投资者
         require(msg.sender != _item);
-
-        Stage storage curStage = _stages[_currentStageIdx];
 
         // 当期投票不同意
         require(!Investor.isVoteAgreeAchieveTarget(curStage.investors, curStage.targetAgreeRate));
@@ -123,14 +106,27 @@ contract StagesToken is ERC20, ERC20Detailed, ERC20Burnable {
         Investor.investorWithdrawABS(curStage.investors, addSendersABS);
     }
 
-    // 结束后: 参照投票结果(失败),项目方领取众筹阶段付出的token
-    function ItemWithdrawToken() public {
-        require(StageTime.isEnd(_stages[_currentStageIdx].stageTime, now));
+    // 项目方: (投票成功) 获取ABS
+    function ItemWithdrawABS() public {
+        Stage storage curStage = _stages[_currentStageIdx];
+        require(StageTime.isEnd(curStage.stageTime, now));
 
         // 只能是项目方
         require(msg.sender == _item);
 
+        // 当前投票同意
+        require(Investor.isVoteAgreeAchieveTarget(curStage.investors, curStage.targetAgreeRate));
+
+        Investor.itemWithdrawABS(curStage.investors, addSendersABS);
+    }
+
+    // 项目方: (投票失败) 获取token
+    function ItemWithdrawToken() public {
         Stage storage curStage = _stages[_currentStageIdx];
+        require(StageTime.isEnd(curStage.stageTime, now));
+
+        // 只能是项目方
+        require(msg.sender == _item);
 
         // 当期投票不同意
         require(!Investor.isVoteAgreeAchieveTarget(curStage.investors, curStage.targetAgreeRate));
@@ -155,14 +151,14 @@ contract StagesToken is ERC20, ERC20Detailed, ERC20Burnable {
         _balances[id] = _balances[id].add(tokenNum);
     }
 
-    // 增加发送者的ABS
-    function addSendersABS(uint256 ABSNum) private {
-        msg.sender.transfer(ABSNum);
-    }
-
     // 增加发送者的token
     function addSendersToken(uint256 tokenNum) private {
         addBalance(msg.sender, tokenNum);
+    }
+
+    // 增加发送者的ABS
+    function addSendersABS(uint256 ABSNum) private {
+        msg.sender.transfer(ABSNum);
     }
 
     constructor (
